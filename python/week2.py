56b5d9657412c86ccba070ea3d33f099e683de1a
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-09-24 19:04:18 -0400

    First Commit

---
+++ python/week2.py
@@ -0,0 +1,38 @@
+
+print(2*(13+4))
+print(5 / 3)
+print( 4/5)
+
+print (0.9 + 0.3)
+print ("hello everyone")
+print ( 3.5 % 7)
+print('I told my mom i want a cat')
+print('MOm: \n\t\ "no\"')
+
+#cost(10)
+#print ("cost of eggs are:" +str(cost))
+#print ("cost of 4 eggs are:", int(price)*4)
+#print ("cost of 5 eggs are: "+ str(int(print)*4))
+
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+print ("hello\n" *4 )
+
+"1" + "9" *4
+
+my_var= "ansh"
+ 
+x = 5
+type("x")
+print("x")
+
