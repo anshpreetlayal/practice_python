5d0d4328dbafb17e9fa3bbb0cc9c422ec5d72e04
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-10-15 16:09:34 -0400

    editing

--- python/record.py
+++ python/record.py
@@ -14,4 +14,19 @@ students = {
             'Art': 'In Progress',
         }
     },
-    
+     2: {
+        'id': 2,
+        'name': 'Bob',
+        'age': 21,
+        'school_name': 'ABC School',
+        'completed_courses': {
+            'Math': 85,
+            'Science': 92,
+            'History': 78,
+        },
+        'ongoing_courses': {
+            'English': 'In Progress',
+            'Gym': 'In Progress',
+        }
+    },
+}
