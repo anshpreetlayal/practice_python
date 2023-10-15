3f133a7780b7fd653e28023e0bd039c2ad51600a
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-10-15 16:08:59 -0400

    changes

---
+++ python/record.py
@@ -0,0 +1,17 @@
+students = {
+    1: {
+        'id': 1,
+        'name': 'Alice',
+        'age': 20,
+        'school_name': 'XYZ School',
+        'completed_courses': {
+            'Math': 90,
+            'Science': 88,
+            'History': 75,
+        },
+        'ongoing_courses': {
+            'English': 'In Progress',
+            'Art': 'In Progress',
+        }
+    },
+    
