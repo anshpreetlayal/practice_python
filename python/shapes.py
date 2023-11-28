2476b0709d001bda2a43d1401dd01d5968c43143
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-27 19:28:57 -0500

    edit

--- python/shapes.py
+++ python/shapes.py
@@ -11,3 +11,16 @@ width = float(input("Enter the width of the rectangle: "))
 #  area of the rectangle
 area_of_rectangle = calculate_rectangle_area(length, width)
 print(f"The area of the rectangle with length {length} and width {width} is: {area_of_rectangle}")
+
+#Area of cirlce
+
+#Area of triangle
+
+# Area of square
+def calculate_square_area(side):
+    area = side**2
+    return area
+
+
+# Taking user input for the side of the square
+side = float(input("Enter the side length of the square: "))
