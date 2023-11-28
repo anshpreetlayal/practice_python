977556ffb01edc730f7d6c0b82aac2dc2f689984
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-27 19:31:18 -0500

    practice

--- python/shapes.py
+++ python/shapes.py
@@ -23,6 +23,9 @@ def calculate_triangle_area(base, height):
 base = float(input("Enter the base of the triangle: "))
 height = float(input("Enter the height of the triangle: "))
 
+# Calculating the area of the triangle
+area_of_triangle = calculate_triangle_area(base, height)
+print(f"The area of the triangle with base {base} and height {height} is: {area_of_triangle}")
 
 # Area of square
 def calculate_square_area(side):
